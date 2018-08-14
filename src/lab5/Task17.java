d00c38d366bbc7b716bb19dca79f8f84580a574f
Artem <salita2212@gmail.com>
2018-08-14 10:04:08 +0600

    сделаны задания 6-10 13-17 в лабораторной 5

---
+++ src/lab5/Task17.java
@@ -0,0 +1,23 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class Task17 {
+
+    public static void main(String[] args) {
+
+
+        int X;
+        int Y;
+        int Z;
+
+        Scanner s = new Scanner(System.in);
+
+        X = s.nextInt();
+        Y = s.nextInt();
+        Z = s.nextInt();
+
+        if(X<Y);
+
+    }
+}
