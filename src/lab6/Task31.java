d819d63fe6db9b7836b20adc8b40a8f91e45b5f1
Artem <salita2212@gmail.com>
2018-08-28 00:05:11 +0600

    решены 21,25,28,29,30   в работе 26,31

---
+++ src/lab6/Task31.java
@@ -0,0 +1,29 @@
+package lab6;
+
+import java.util.Scanner;
+
+public class Task31 {
+
+    public static void main(String[] args) {
+
+
+        int Y;
+        int a = 0;
+
+        Scanner s = new Scanner(System.in);
+        Y = s.nextInt();
+
+        if (Y <= 0) {
+            System.out.println("Значение Y должно быть положительным");
+            return;
+        }
+
+
+
+
+
+    }
+
+}
+
+
